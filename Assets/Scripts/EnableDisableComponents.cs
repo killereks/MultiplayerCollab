2c2a3935051a4df76b5a170898319085484e8b72
killereks <ssneeff@gmail.com>
2021-05-13 16:57:02 +0100

    fix canvas bug

--- Assets/Scripts/EnableDisableComponents.cs
+++ Assets/Scripts/EnableDisableComponents.cs
@@ -8,12 +8,22 @@ public class EnableDisableComponents : NetworkBehaviour {
     public GameObject[] objectsToDisableIfMine;
     public GameObject[] objectsToEnableIfMine;
 
+    public GameObject[] objectsToDisableIfNotMine;
+    public GameObject[] objectsToEnableIfNotMine;
+
     private void Start() {
         if (isLocalPlayer) {
             foreach (GameObject obj in objectsToDisableIfMine) {
                 obj.SetActive(false);
             }
             foreach (GameObject obj in objectsToEnableIfMine) {
+                obj.SetActive(true);
+            }
+        } else {
+            foreach (GameObject obj in objectsToDisableIfNotMine) {
+                obj.SetActive(false);
+            }
+            foreach (GameObject obj in objectsToEnableIfNotMine) {
                 obj.SetActive(true);
             }
         }
